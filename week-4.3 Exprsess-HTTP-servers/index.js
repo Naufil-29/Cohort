2b8aa7a6782b92b69f6bda924cf32d8d1414ff53
Shaikh_Naufil <shaikhnaufil2908@gmail.com>
2025-11-01 17:13:51 +0530

    week-4.3(Express-HTTP-Servers)

---
+++ week-4.3 Exprsess-HTTP-servers/index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+
+var users = [{ 
+    name: "John",
+    kidneys: [
+        { no: 1, healthy: false },    ]
+}];
+
+app.use(express.json());
+
+app.get("/", function(req, res) { 
+    const johnKidneys = users[0].kidneys;
+    const numberOfKidneys = johnKidneys.length;
+
+    let numberOfHealthyKidneys = 0;
+    for (let i = 0; i < johnKidneys.length; i++) { 
+        if (johnKidneys[i].healthy) { 
+            numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+        }
+    }
+
+    const numberOfUnHealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+
+    res.json({ 
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnHealthyKidneys,
+    });
+});
+
+app.post("/", function(req, res){ 
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({ 
+        healthy: isHealthy
+    });
+
+    res.json({ 
+        msg:"done"
+    });
+});
+
+app.put("/", function(req, res){ 
+    for(let i = 0; i < users.length; i++){ 
+        users[0].kidneys[i].healthy = true;
+    };
+    res.json({})
+});
+
+app.delete("/", function(req, res) {
+        if(isThereAtleastOnebadKidney()){ 
+
+        
+        const newKidneys = [];
+        for (let i = 0; i<users[0].kidneys.length; i++) {
+            if (users[0].kidneys[i].healthy) {
+                newKidneys.push({
+                    healthy: true
+                })
+            }
+        }
+        users[0].kidneys = newKidneys;
+        res.json({msg: "done"})   
+    }
+    else( 
+        res.status(411).json({msg:"no bad kidney found"})
+    )
+    });
+
+    function isThereAtleastOnebadKidney(){ 
+        let atleastOneBadKidney = false;
+        for(let i = 0; i<users[0].kidneys.length; i++){ 
+            if(!users[0].kidneys[i].healthy){ 
+                atleastOneBadKidney = true;
+            };
+        };
+        return atleastOneBadKidney;
+    }
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log("Server running on port 3000");
+});
+

78b6795bfd400d0bb244e76b5de2a2239a8ac891
Shaikh_Naufil <shaikhnaufil2908@gmail.com>
2025-11-04 17:09:49 +0530

    week-5.1(Headers,query,params)

---
+++ week-5.1(Headers, QueryParams and Express)/HTTP-SERVER/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.get("/add", (req, res) => { 
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+       res.json({ 
+        ans :  a + b
+       });
+});
+
+app.get("/subtract", (req, res) => { 
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+       res.json({ 
+        ans :  a - b
+       });
+});
+
+app.get("/multiply", (req, res) => { 
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+       res.json({ 
+        ans :  a * b
+       });
+});
+
+app.get("/divide", (req, res) => { 
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+       res.json({ 
+        ans :  a / b
+       });
+});
+
+app.listen(3000, console.log("app is running on port 3000"))

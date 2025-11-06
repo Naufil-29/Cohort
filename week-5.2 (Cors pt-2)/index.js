35c61f005881351072d363c6e2aefcbfdbaf5312
Shaikh_Naufil <shaikhnaufil2908@gmail.com>
2025-11-06 17:20:29 +0530

    week-5.2(pt-2 cors)

---
+++ week-5.2 (Cors pt-2)/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+     res.sendFile(__dirname + "/public/index.html")
+})
+
+app.post("/sum", (req, res) => { 
+    const a = parseInt(req.body.a);
+    const b = parseInt(req.body.b);
+
+    res.json({ 
+       answer : a + b,
+    });
+});
+
+app.listen(3001, console.log("app is running on 3001"));

461dcc69abe35c5d0eca36b365d1f72d67ed0e1a
Shaikh_Naufil <shaikhnaufil2908@gmail.com>
2025-12-08 13:12:27 +0530

    week-14.1(Typescript)

---
+++ week-14.1(TypeScript)/index.js
@@ -0,0 +1,41 @@
+"use strict";
+// function greeting(firstName: string){
+//    console.log(`hello ${firstName}`)
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+// greeting("Naufil");
+// function sum(x:number, y:number){ 
+//    return x + y;
+// }
+// let ans = sum(10, 20);
+// console.log(sum)
+// function delayedFunc(fn: () => void){ 
+//    setTimeout(fn, 1000)
+// }
+// function toCall(){ 
+//    console.log("1s Passed")
+// }
+// delayedFunc(toCall)
+// function welcome(user:{
+//     name: string,
+//     age: number
+// }){ 
+//    console.log(` hello ${user.name}, I am ${user.age} old`)
+// }
+// welcome({ 
+//    name: "Naufil",
+//    age:20
+// })
+function isLegal(user) {
+    if (user.age > 18) {
+        console.log("londa bada hogya");
+    }
+    else {
+        console.log("abe tu abhi baccha he");
+    }
+}
+isLegal({
+    name: "harkirat",
+    age: 17
+});
+//# sourceMappingURL=index.js.map
---
+++ week-14.1(TypeScript)/index.ts
@@ -0,0 +1,54 @@
+// function greeting(firstName: string){
+//    console.log(`hello ${firstName}`)
+// }
+
+// greeting("Naufil");
+
+// function sum(x:number, y:number){ 
+//    return x + y;
+// }
+
+// let ans = sum(10, 20);
+// console.log(sum)
+
+// function delayedFunc(fn: () => void){ 
+//    setTimeout(fn, 1000)
+// }
+
+// function toCall(){ 
+//    console.log("1s Passed")
+// }
+
+// delayedFunc(toCall)
+
+// function welcome(user:{
+//     name: string,
+//     age: number
+// }){ 
+//    console.log(` hello ${user.name}, I am ${user.age} old`)
+// }
+
+// welcome({ 
+//    name: "Naufil",
+//    age:20
+// })
+
+
+
+
+function isLegal(user:{ 
+   name: string,
+   age: number
+}){ 
+   if(user.age > 18){ 
+      console.log("londa bada hogya")
+   }
+   else{ 
+      console.log("abe tu abhi baccha he")
+   }
+}
+
+isLegal({ 
+   name: "harkirat",
+   age: 21
+})

d699acc74e8697f79d5ea0d9b97f46c11db15154
Shaikh_Naufil <shaikhnaufil2908@gmail.com>
2025-12-30 16:44:23 +0530

    week-18.1(Prisma and ORMs)

---
+++ week-18.1(Prisma and ORMs)/dist/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from "./generated/prisma/client.js";
+const app = express();
+express.json();
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const client = new PrismaClient({ adapter });
+app.get("/users", async (req, res) => {
+    const users = await client.user.findMany();
+    res.json({
+        users
+    });
+});
+async function createUser() {
+    await client.user.create({
+        data: {
+            username: "Naufil",
+            password: "naufil1234",
+            age: 20,
+            city: "amd"
+        }
+    });
+}
+createUser();
+app.listen(3000);
+//# sourceMappingURL=index.js.map
